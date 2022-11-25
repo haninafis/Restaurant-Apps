f30550838643ed2533117e6dd13b699b85f890ff
haninafis <haninafis@gmail.com>
2022-11-25 21:48:19 +0700

    Add Drawer

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,11 +12,29 @@ import ('../DATA.json').then(({default: jsonData}) => {
             <div class="city">${data['city']}</div>
             <div class="list_item_detail">
                 <p class="list_item_rating">Rating : <a class="list_item_rating_value">${data['rating']}</a></p>
-                <h2 class="list_item_title">${data['name']}</a></h2>
+                <h2 class="list_item_title"><a href="#">${data['name']}</a></h2>
                 <div class="list_item_description">${data['description'].slice(0, 175)}...</div>
             </div>
         </div>
         `;
     });
     document.querySelector('#list').innerHTML = dataList;  
+});
+
+const menu = document.querySelector('#menu');
+const hero = document.querySelector('.heroelement');
+const main = document.querySelector('main');
+const drawer = document.querySelector('#drawer');
+
+menu.addEventListener('click', function (event) {
+    drawer.classList.toggle('open');
+    event.stopPropagation();
+});
+
+hero.addEventListener('click', function () {
+    drawer.classList.remove('open');
+});
+
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
 });
