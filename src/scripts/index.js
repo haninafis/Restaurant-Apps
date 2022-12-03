ab91ff22a9a793c4eac354ed01c9edb5c80342e5
Hani Nafis Muzaki <haninafis@gmail.com>
2022-12-03 20:05:01 +0700

    Add URL Router

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,28 +2,18 @@ import 'regenerator-runtime';
 import '../styles/main.css';
 import '../styles/responsive.css';
 import App from './views/app';
-import list from '../DATA.json';
-
-const datas = list.restaurants;
-let dataList = '';
-datas.forEach((data) => {
-  dataList += `
-    <div class="list_item">
-        <img class="list_item_img" src="${data.pictureId}" alt="${data.name}" title="${data.name}">
-        <div class="city">${data.city}</div>
-        <div class="list_item_detail">
-            <p class="list_item_rating">Rating : <a href="#" class="list_item_rating_value">${data.rating}</a></p>
-            <h2 class="list_item_title"><a href="#">${data.name}</a></h2>
-            <div class="list_item_description">${data.description.slice(0, 125)}...</div>
-        </div>
-    </div>
-    `;
-});
-document.querySelector('#list').innerHTML = dataList;
 
 // eslint-disable-next-line no-unused-vars
 const app = new App({
   button: document.querySelector('#menu'),
   drawer: document.querySelector('#navigationDrawer'),
   content: document.querySelector('#content'),
+});
+
+window.addEventListener('hashchange', () => {
+  app.renderPage();
+});
+
+window.addEventListener('load', () => {
+  app.renderPage();
 });
