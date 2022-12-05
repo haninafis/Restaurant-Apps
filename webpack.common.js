e29ad0ae61ba74801db6fc6a4232b705141d24b5
Hani Nafis Muzaki <haninafis@gmail.com>
2022-12-05 13:58:16 +0700

    Add Service Worker

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,10 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/scripts/index.js'),
+  entry: {
+    app: path.resolve(__dirname, 'src/scripts/index.js'),
+    sw: path.resolve(__dirname, 'src/scripts/sw.js'),
+  },
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
