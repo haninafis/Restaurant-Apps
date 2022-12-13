9defbe411262f321bec64e79603ddef5675557dd
Hani Nafis Muzaki <haninafis@gmail.com>
2022-12-13 19:39:59 +0700

    Ketika Pikiranku Berubah

---
+++ specs/unlikeRestoSpec.js
@@ -0,0 +1,71 @@
+import LikeButtonInitiator from '../src/scripts/utils/like-button-initiator';
+import FavoriteRestoIdb from '../src/scripts/data/favorite-resto-idb';
+
+const addLikeButtonContainer = () => {
+  document.body.innerHTML = '<div id="likeButtonContainer"></div>';
+};
+
+describe('unliking a resto', () => {
+  beforeEach(async () => {
+    addLikeButtonContainer();
+    await FavoriteRestoIdb.putResto({ id: 1 });
+  });
+
+  afterEach(async () => {
+    await FavoriteRestoIdb.deleteResto(1);
+  });
+
+  it('should display unlike widget when the resto has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="unlike this resto"]'))
+      .toBeTruthy();
+  });
+
+  it('should not display like widget when the resto has been liked', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    expect(document.querySelector('[aria-label="like this resto"]'))
+      .toBeFalsy();
+  });
+
+  it('should be able to remove liked resto from the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    document.querySelector('[aria-label="unlike this resto"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestoIdb.getAllRestos()).toEqual([]);
+  });
+
+  it('should not throw error if the unliked resto is not in the list', async () => {
+    await LikeButtonInitiator.init({
+      likeButtonContainer: document.querySelector('#likeButtonContainer'),
+      resto: {
+        id: 1,
+      },
+    });
+
+    // hapus dulu resto dari daftar resto yang disukai
+    await FavoriteRestoIdb.deleteResto(1);
+
+    // kemudian, simulasikan pengguna menekan widget batal menyukai resto
+    document.querySelector('[aria-label="unlike this resto"]').dispatchEvent(new Event('click'));
+
+    expect(await FavoriteRestoIdb.getAllRestos()).toEqual([]);
+  });
+});
