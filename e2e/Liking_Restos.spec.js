96a844d7ef485c8d2d9d44aeb9ff65d299773e05
Hani Nafis Muzaki <haninafis@gmail.com>
2022-12-14 18:53:08 +0700

    Membekukan waktu

--- e2e/Liking_Restos.spec.js
+++ e2e/Liking_Restos.spec.js
@@ -16,3 +16,18 @@ Scenario('liking one resto', async ({ I }) => {
     I.amOnPage('/');
     // … kita akan mengisi uji coba berikutnya …
 });
+
+Scenario('liking one resto', ({ I }) => {
+    I.see('Tidak ada resto untuk ditampilkan', '.resto_not_found');
+  
+    I.amOnPage('/');
+    
+    I.seeElement('.list_item_title a');
+    I.click(locate('.list_item_title a').first());
+
+    I.seeElement('#likeButton');
+    I.click('#likeButton');
+  
+    I.amOnPage('/#/like');
+    I.seeElement('.list_item');
+});
